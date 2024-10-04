7cb90fce75984ed7edb354169a36ab4b1e75c483
Yunlincla <k5711628@gmail.com>
2024-10-05 00:18:04 +0800

    添加swift-http-type依賴

--- Package.swift
+++ Package.swift
@@ -9,13 +9,20 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LYFetcher",
-            targets: ["LYFetcher"]),
+            targets: ["LYFetcher"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-http-types.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LYFetcher"),
+            name: "LYFetcher",
+            dependencies: [
+                .product(name: "HTTPTypes", package: "swift-http-types"),
+                .product(name: "HTTPTypesFoundation", package: "swift-http-types")
+            ]),
         .testTarget(
             name: "LYFetcherTests",
             dependencies: ["LYFetcher"]),
