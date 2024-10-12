dff9ec9febcf4fcc2bf2aef93fe8dc64d400c1da
Yunlincla <k5711628@gmail.com>
2024-10-12 14:16:43 +0800

    移除swift-http-type依賴。

--- Package.swift
+++ Package.swift
@@ -17,18 +17,11 @@ let package = Package(
             name: "LYFetcher",
             targets: ["LYFetcher"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-http-types.git", from: "1.0.0")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LYFetcher",
-            dependencies: [
-                .product(name: "HTTPTypes", package: "swift-http-types"),
-                .product(name: "HTTPTypesFoundation", package: "swift-http-types")
-            ]),
+            name: "LYFetcher"),
         .testTarget(
             name: "LYFetcherTests",
             dependencies: ["LYFetcher"]),
